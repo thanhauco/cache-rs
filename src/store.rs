a74c6197fd502b9804cd956a7f1c2b4609628144
Thanh Vu <thanhauco@gmail.com>
2021-01-13 07:16:00 -0800

    Implement Set/Get

--- src/store.rs
+++ src/store.rs
@@ -6,4 +6,10 @@ impl Store {
     pub fn new() -> Self {
         Store { data: HashMap::new() }
     }
+    pub fn set(&mut self, key: String, value: String) {
+        self.data.insert(key, value);
+    }
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.data.get(key)
+    }
 }
