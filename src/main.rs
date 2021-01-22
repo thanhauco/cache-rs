be4cc011c2d3084db71c275844c313477f641844
Thanh Vu <thanhauco@gmail.com>
2021-01-22 11:14:00 -0800

    Handle connections

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod store;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::thread;
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    stream.write(&buffer).unwrap();
+}
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        stream.write(b"Hello").unwrap();
+        let stream = stream.unwrap();
+        thread::spawn(|| handle_client(stream));
     }
 }
