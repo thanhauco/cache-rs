5aeace28b6379c5437d0cb24009ddf9c926a3929
Thanh Vu <thanhauco@gmail.com>
2021-01-27 05:16:00 -0800

    Parse Command

--- src/command.rs
+++ src/command.rs
@@ -3,3 +3,13 @@ pub enum Command {
     Get(String),
     Unknown,
 }
+impl Command {
+    pub fn parse(input: &str) -> Command {
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
+        match parts.as_slice() {
+            ["SET", k, v] => Command::Set(k.to_string(), v.to_string()),
+            ["GET", k] => Command::Get(k.to_string()),
+            _ => Command::Unknown,
+        }
+    }
+}
