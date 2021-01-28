683429e3d8bea929240b0ae2365a3d88f98d5a17
Thanh Vu <thanhauco@gmail.com>
2021-01-27 21:30:00 -0800

    Share Store with Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 mod store;
+mod command;
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::sync::{Arc, Mutex};
 use std::thread;
-fn handle_client(mut stream: TcpStream) {
+use store::Store;
+use command::Command;
+fn handle_client(mut stream: TcpStream, store: Arc<Mutex<Store>>) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    stream.write(&buffer).unwrap();
+    let req = String::from_utf8_lossy(&buffer[..]);
+    let resp = match Command::parse(&req) {
+        Command::Set(k, v) => {
+            store.lock().unwrap().set(k, v);
+            "OK".to_string()
+        },
+        Command::Get(k) => {
+            match store.lock().unwrap().get(&k) {
+                Some(v) => v.clone(),
+                None => "(nil)".to_string(),
+            }
+        },
+        Command::Unknown => "ERR".to_string(),
+    };
+    stream.write(resp.as_bytes()).unwrap();
 }
 fn main() {
+    let store = Arc::new(Mutex::new(Store::new()));
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| handle_client(stream));
+        let store = store.clone();
+        thread::spawn(|| handle_client(stream, store));
     }
 }
