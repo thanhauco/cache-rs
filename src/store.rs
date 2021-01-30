95700c4df1e8f48207141def33e0984ce43f815c
Thanh Vu <thanhauco@gmail.com>
2021-01-29 20:37:00 -0800

    Add TTL to Store

--- src/store.rs
+++ src/store.rs
@@ -1,15 +1,30 @@
 use std::collections::HashMap;
+use std::time::{Instant, Duration};
+struct Entry {
+    val: String,
+    exp: Option<Instant>,
+}
 pub struct Store {
-    data: HashMap<String, String>,
+    data: HashMap<String, Entry>,
 }
 impl Store {
     pub fn new() -> Self {
         Store { data: HashMap::new() }
     }
-    pub fn set(&mut self, key: String, value: String) {
-        self.data.insert(key, value);
+    pub fn set(&mut self, key: String, value: String, ttl: u64) {
+        let exp = if ttl > 0 { Some(Instant::now() + Duration::from_secs(ttl)) } else { None };
+        self.data.insert(key, Entry { val: value, exp });
     }
-    pub fn get(&self, key: &str) -> Option<&String> {
-        self.data.get(key)
+    pub fn get(&mut self, key: &str) -> Option<String> {
+        if let Some(e) = self.data.get(key) {
+            if let Some(exp) = e.exp {
+                if Instant::now() > exp {
+                    self.data.remove(key);
+                    return None;
+                }
+            }
+            return Some(e.val.clone());
+        }
+        None
     }
 }
