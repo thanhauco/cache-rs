4011d71a9d878ab24d2777d45ff742103429c881
Thanh Vu <thanhauco@gmail.com>
2021-01-31 12:43:00 -0800

    Update Command for TTL

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,5 @@
 pub enum Command {
-    Set(String, String),
+    Set(String, String, u64),
     Get(String),
     Unknown,
 }
@@ -7,7 +7,8 @@ impl Command {
     pub fn parse(input: &str) -> Command {
         let parts: Vec<&str> = input.trim().split_whitespace().collect();
         match parts.as_slice() {
-            ["SET", k, v] => Command::Set(k.to_string(), v.to_string()),
+            ["SET", k, v, ttl] => Command::Set(k.to_string(), v.to_string(), ttl.parse().unwrap_or(0)),
+            ["SET", k, v] => Command::Set(k.to_string(), v.to_string(), 0),
             ["GET", k] => Command::Get(k.to_string()),
             _ => Command::Unknown,
         }
