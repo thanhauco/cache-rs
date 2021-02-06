8e90d29e09b24beb9a29a20034241bd42c2238e9
Thanh Vu <thanhauco@gmail.com>
2021-02-05 16:30:00 -0800

    Add Save Command

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,7 @@
 pub enum Command {
     Set(String, String, u64),
     Get(String),
+    Save,
     Unknown,
 }
 impl Command {
@@ -10,6 +11,7 @@ impl Command {
             ["SET", k, v, ttl] => Command::Set(k.to_string(), v.to_string(), ttl.parse().unwrap_or(0)),
             ["SET", k, v] => Command::Set(k.to_string(), v.to_string(), 0),
             ["GET", k] => Command::Get(k.to_string()),
+            ["SAVE"] => Command::Save,
             _ => Command::Unknown,
         }
     }
