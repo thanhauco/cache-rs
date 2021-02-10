ae03a9c330b92e65fd7ee7ed6e1a8e001e09b76a
Thanh Vu <thanhauco@gmail.com>
2021-02-10 15:07:00 -0800

    Add LRU Logic

--- src/store.rs
+++ src/store.rs
@@ -5,26 +5,34 @@ use std::io::Write;
 struct Entry {
     val: String,
     exp: Option<Instant>,
+    last_access: Instant,
 }
 pub struct Store {
     data: HashMap<String, Entry>,
+    capacity: usize,
 }
 impl Store {
     pub fn new() -> Self {
-        Store { data: HashMap::new() }
+        Store { data: HashMap::new(), capacity: 100 }
     }
     pub fn set(&mut self, key: String, value: String, ttl: u64) {
+        if self.data.len() >= self.capacity {
+            // Simple random eviction for now
+            let k = self.data.keys().next().unwrap().clone();
+            self.data.remove(&k);
+        }
         let exp = if ttl > 0 { Some(Instant::now() + Duration::from_secs(ttl)) } else { None };
-        self.data.insert(key, Entry { val: value, exp });
+        self.data.insert(key, Entry { val: value, exp, last_access: Instant::now() });
     }
     pub fn get(&mut self, key: &str) -> Option<String> {
-        if let Some(e) = self.data.get(key) {
+        if let Some(e) = self.data.get_mut(key) {
             if let Some(exp) = e.exp {
                 if Instant::now() > exp {
                     self.data.remove(key);
                     return None;
                 }
             }
+            e.last_access = Instant::now();
             return Some(e.val.clone());
         }
         None
