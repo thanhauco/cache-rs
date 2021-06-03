be63d84c7c5b90b3c85bdbe69efec1aaca365d54
Thanh Vu <thanhauco@gmail.com>
2021-06-03 09:48:00 -0700

    Use Jemalloc

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-mod store;
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-#[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
-    println!("Async Server Started");
-}
+use jemallocator::Jemalloc;
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
