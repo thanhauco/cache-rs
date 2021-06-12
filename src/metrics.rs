0eb2e1acae2facf8815de4e1260ca3ac1b1c23d3
Thanh Vu <thanhauco@gmail.com>
2021-06-12 15:41:00 -0700

    Add Prometheus Exporter

---
+++ src/metrics.rs
@@ -0,0 +1,8 @@
+use prometheus::{Encoder, TextEncoder};
+pub fn gather() -> String {
+    let encoder = TextEncoder::new();
+    let metric_families = prometheus::gather();
+    let mut buffer = vec![];
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    String::from_utf8(buffer).unwrap()
+}
