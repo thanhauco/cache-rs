20aa3b65d1ce047fb97c46b83d43e654af14cbf9
Thanh Vu <thanhauco@gmail.com>
2022-01-27 14:31:00 -0800

    Add Metrics Exporter

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,8 +1,4 @@
-use prometheus::{Encoder, TextEncoder};
-pub fn gather() -> String {
-    let encoder = TextEncoder::new();
-    let metric_families = prometheus::gather();
-    let mut buffer = vec![];
-    encoder.encode(&metric_families, &mut buffer).unwrap();
-    String::from_utf8(buffer).unwrap()
+use metrics_exporter_prometheus::PrometheusBuilder;
+fn init() {
+    PrometheusBuilder::new().install().expect("failed to install recorder");
 }
